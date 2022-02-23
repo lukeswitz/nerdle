66f10da0bd470078eafbd2d88b5684cccaee2a0e
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2022-02-23 10:58:42 -0500

    fix: :wrench: timer sync issue

--- src/components/widgets/Timer.svelte
+++ src/components/widgets/Timer.svelte
@@ -10,17 +10,16 @@
 	const HOUR = 3600000;
 	const MINUTE = 60000;
 	const SECOND = 1000;
-	const OFFSET = 25200000;
 	let ms = 1000;
 
 	let countDown: number;
 
 	export function reset(m: GameMode) {
 		clearInterval(countDown);
-		ms = modeData.modes[m].unit - (new Date().valueOf() - modeData.modes[m].seed - OFFSET);
+		ms = modeData.modes[m].unit - (new Date().valueOf() - modeData.modes[m].seed - (new Date().getTimezoneOffset() * 60000));
 		if (ms < 0) dispatch("timeup");
 		countDown = setInterval(() => {
-			ms = modeData.modes[m].unit - (new Date().valueOf() - modeData.modes[m].seed - OFFSET);
+			ms = modeData.modes[m].unit - (new Date().valueOf() - modeData.modes[m].seed - (new Date().getTimezoneOffset() * 60000));
 			if (ms < 0) {
 				clearInterval(countDown);
 				dispatch("timeup");
