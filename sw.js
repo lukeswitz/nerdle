6f129e9f42c58a1a33613198cc4d4fa6266e16a4
lukeswitz <lukeswitz@users.noreply.github.com>
2022-06-15 22:59:30 +0000

    deploy: 2422c96ee87f107cc9fa9ede0db91a1b4f65d3f5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const cacheName = "nerdle-v1.3.0";
+const cacheName = "nerdle-v1.3.1";
 
 const assetsToCache = [
 	"./",
